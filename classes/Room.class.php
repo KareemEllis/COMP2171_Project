bb742bab3476b9cf130da5c001df28b570bf9b7e
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-03-20 21:56:27 -0500

    Implemented Room class

---
+++ classes/Room.class.php
@@ -0,0 +1,63 @@
+<?php
+
+class Room{
+
+    private $roomNumber;
+    private $roomType;
+    private $status;
+    private $block;
+    private $resident1;
+    private $resident2;
+
+    public function __construct($roomNumber, $roomType, $status, $block, $resident1, $resident2){
+
+        $this->roomNumber = $roomNumber;
+        $this->roomType = $roomType;
+        $this->status = $status;
+        $this->block = $block;
+        $this->resident1 = $resident1;
+        $this->resident2 = $resident2;
+    }
+    
+    public function getRoomNumber(){
+        return $this->roomNumber;
+    }
+
+    public function getRoomType(){
+        return $this->roomType;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+
+    public function getBlock(){
+        return $this->block;
+    }
+
+    public function getResident1(){
+        return $this->resident1;
+    }
+
+    public function getResident2(){
+        return $this->resident2;
+    }
+
+    //Set Room Status
+    public function setStatus($newStatus){
+        $this->status = $newStatus;
+    }
+
+    public function __toString()
+    {
+        echo "Room Number: ".$this->getRoomNumber();
+        echo "Room Type: ".$this->getRoomType();
+        echo "Block: ".$this->getBlock();
+        echo "Resident #1: ".$this->getResident1();
+        echo "Resident #2: ".$this->getResident2();
+        echo "Status: ".$this->getStatus();
+    }
+
+}
+
+?>
