803fb8a6bb14c1ceeb96f65e293d4bdb9bb11aec
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-09 02:40:54 -0500

    Implemented feature to remove residents

--- pages/roomDetails.php
+++ pages/roomDetails.php
@@ -50,6 +50,10 @@ if(isset($_POST['submit'])){
     
 }
 
+if (isset($_POST['remove'])){
+    $roomManager->removeResidents($roomNum);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -128,11 +132,19 @@ if(isset($_POST['submit'])){
                                 <input type="text" name='res1' value=" " placeholder="eg. 1">
                             </div>
                             
-                            <div class="assignBtn_div">
-                                <input class= "assignBtn" name= "submit" type="submit" value="Assign Resident">
+                            <div class="buttonBox">
+                                <div class="assignBtn_div">
+                                    <input class= "assignBtn" name= "submit" type="submit" value="Assign Resident">
+                                </div>
+
+                                <div class="removeBtn_div">
+                                    <input class= "removeBtn" name= "remove" type="submit" value="Remove Current Residents">
+                                </div>
                             </div>
                             
+                            
                         </form>
+
 
                         <?php endif; ?>
 
@@ -153,9 +165,16 @@ if(isset($_POST['submit'])){
                                 <input type="text" name='res2' placeholder="eg. 2">
                             </div>
                             
-                            <div class="assignBtn_div">
-                                <input class= "assignBtn" name= "submit" type="submit" value="Assign Residents">
+                            <div class="buttonBox">
+                                <div class="assignBtn_div">
+                                    <input class= "assignBtn" name= "submit" type="submit" value="Assign Resident">
+                                </div>
+
+                                <div class="removeBtn_div">
+                                    <input class= "removeBtn" name= "remove" type="submit" value="Remove Current Residents">
+                                </div>
                             </div>
+                            
                         </form>
 
 
