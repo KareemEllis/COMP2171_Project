8d92a96c7c67771eff4b7c31a4554d912751e06e
Kareem Ellis <kareemellis55@gmail.com>
2023-04-11 01:18:50 -0500

    Styled index.php

--- pages/index.php
+++ pages/index.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Landing Page</title>
+	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    Front Page
-    <a href="./applicationForm.php">Application Form</a>
-    <a href="./dashboard.php">Dashboard (Need to implement login)</a>
+	<header>
+		<h1>Home Page</h1>
+		<nav>
+			<ul>          
+                
+			</ul>
+		</nav>
+	</header>
+
+	<section class="hero">
+		<h2>George Alleyne Hall</h2>
+		<div class="buttons">
+			<a href="./dashboard.php" class="login-button">Login</a>
+			<a href="./applicationForm.php" class="apply-button">Apply Now</a>
+		</div>
+	</section>
+
 </body>
 </html>
